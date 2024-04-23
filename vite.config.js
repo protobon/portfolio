051f38de82c4a8c03f6d7eaac5142b08314af6c6
ayrton <ayrtoncoelhods@gmail.com>
2024-04-23 19:09:54 -0300

    vite build for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Output directory for the build process
+    emptyOutDir: true, // Clear the output directory before building
+    base: '/portfolio', // Set the base path for deployed assets (replace with your desired path)
+  },
 })
