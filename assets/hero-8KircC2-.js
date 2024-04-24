55f83f301a68e4e59937c3af7f64d8f136d1060a
ayrton <ayrtoncoelhods@gmail.com>
2024-04-23 21:56:01 -0300

    Updates

--- assets/hero-8KircC2-.js
+++
@@ -1,1 +0,0 @@
-const e=["Greetings! My name is Ayrton and I'm a software developer from Uruguay. =)"],t="hero.jpg",o={text:e,image:t};export{o as default,t as image,e as text};
--- assets/hero-DA2JI65-.js
+++
@@ -1,1 +0,0 @@
-const e=["Saludos! Me llamo Ayrton y soy un desarrollador web de Uruguay. =)"],o="hero.jpg",t={text:e,image:o};export{t as default,o as image,e as text};
